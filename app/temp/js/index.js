5e778ed769185c1b72e1e30763c9ab9d62892e16
Alexey Solonenko <ot2@bk.ru>
2017-08-09 00:43:53 +0200

    feat: Add back buton to game menu

--- app/temp/js/index.js
+++ app/temp/js/index.js
@@ -629,43 +629,99 @@ gd.positionHoverDiv = function(){
 *
 */
 
+gd.gameMenuPrev = [];
+
+gd.gameMenuSetPrev = function(dontSetPrev) {
+  if(!dontSetPrev){
+    var el = document.getElementsByClassName('game-menu-modal-body')[0];
+    var classList = [];
+    for(var i=0;i<el.childNodes.length;i++){
+      if(el.childNodes[i].style.display != 'none') {
+        classList = el.childNodes[i].classList;
+      };
+    };
+    
+    for(var i=0; i<classList.length; i++){
+      if(classList[i].search('game-menu')!=-1) {
+        gd.gameMenuPrev.push('.'+classList[i]);
+      };
+    };
+  };
+  
+};
+
+gd.gameMenuErase = function(dontSetPrev) {
+  gd.gameMenuSetPrev(dontSetPrev);
+  $('.game-menu-modal-body > div').hide(250);
+};
+
+
 gd.gameMenuButtonHTML = '<div class="col-xs-%% btn btn-default %class% center-block"><h3>%data%</h3></div>';
 gd.gameMenuContent = [];
-gd.gameMenuContent[0] = {name:'START',itemClass:'gameMenuBtnStart',type:'button',cols:6};
-gd.gameMenuContent[1] = {name:'RULES',itemClass:'gameMenuBtnStart',type:'button',cols:6};
-gd.gameMenuContent[2] = {name:'ABOUT',itemClass:'gameMenuBtnStart',type:'button',cols:6};
-gd.gameMenuContent[3] = {name:'INTRO',itemClass:'gameMenuBtnStart',type:'button',cols:6};
+gd.gameMenuContent[0] = {name:'CONTINUE',itemClass:'game-menu-btn-continue',type:'button',cols:6};
+gd.gameMenuContent[1] = {name:'RULES',itemClass:'game-menu-btn-rules',type:'button',cols:6};
+gd.gameMenuContent[2] = {name:'OPTIONS',itemClass:'game-menu-btn-options',type:'button',cols:6};
+gd.gameMenuContent[3] = {name:'START OVER',itemClass:'game-menu-btn-restart',type:'button',cols:6};
 
-gd.gameMenuBuild = function(){
+gd.gameMenuHome = function() {
   for(var i = 0, width=0;i<gd.gameMenuContent.length;i++){
     var formattedHTML = '';
     formattedHTML = gd.gameMenuButtonHTML.replace('%data%',gd.gameMenuContent[i].name);
     formattedHTML = formattedHTML.replace('%class%',gd.gameMenuContent[i].itemClass);
     formattedHTML = formattedHTML.replace('col-xs-%%','col-xs-'+gd.gameMenuContent[i].cols);
-    $('.gameMenuModal1 .modal-body').append(formattedHTML);
+    $('.game-menu-home').append(formattedHTML);
     width = width + gd.gameMenuContent[i].cols;
     if(width > 11){//||((width + gd.gameMenuContent[i+1].cols)>11)){
-      $('.gameMenuModal1 .modal-body').append('<div class="col-xs-12"></div>');
+      $('.game-menu-home').append('<div class="col-xs-12"></div>');
       width = 0;
     };
   }; 
 };
-gd.gameMenuBuild();
+
+
+
+//TO DO - TO MOVE TO INIT
+gd.gameMenuHome();
+gd.gameMenuErase();
+gd.gameMenuPrev = [];
+
 gd.gameMenuModal1 = '<div class="gameMenuModal1"></div>';
 document.getElementsByClassName('btnMenu')[0].onclick = function(){
-        
-        //document.getElementsByClassName('gameMenuModal1')[0].modal('show');
         if(!gd.paused) { gd.pause();
         } else {  
         };
+        gd.gameMenuErase();
+        $('.game-menu-home').show(500);
+        gd.gameMenuPrev.push('game-menu-home');
         $('.gameMenuModal1').modal('show');
 };
 
 gd.health = 50;
 
+$('.game-menu-btn-options').click(function(){
+  gd.gameMenuErase();
+  $('.game-menu-options').show(500);
+});
+
+$('.game-menu-player-sprite-btn').click(function(){
+  gd.gameMenuErase();
+  $('.game-menu-player-sprite').show(500);
+});
+
+$('.game-menu-btn-prev').click(function(){
+  if(gd.gameMenuPrev.length>1){
+    gd.gameMenuErase(true);
+    $(gd.gameMenuPrev[gd.gameMenuPrev.length-1]).show(500);
+    gd.gameMenuPrev.pop();
+  };
+});
+
+
+/*
 document.getElementsByClassName('btnPlayerSprite')[0].onclick = function(){
    $('.user-modal-player-sprite').modal('show');
 };
+*/
 
 
 
